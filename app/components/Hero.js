aa68040300c8ba9755f664b7486d1b5739a654cd
Lifguson <lwellsteadgi@gmail.com>
2023-05-13 16:31:41 +0200

    fix: centered elements;

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -2,13 +2,13 @@
 
 import { Link } from "react-scroll/modules";
 import { HiArrowDown } from "react-icons/hi";
+import Image from "next/image";
 
 const Hero = () => {
   return (
     <section id="home">
-      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
-        <div>{/* image */}</div>
-        <div className="md:mt-2 md:w-3/5">
+      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4">
+        <div className="md:mt-2">
           <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">
             Hi, I'm Leon
           </h1>
