8a8cbe434185e22846ea25c5e905c47a93f81da9
Lifguson <lwellsteadgi@gmail.com>
2023-05-18 11:28:40 +0200

    feat: added logo svg;

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -5,12 +5,36 @@ import { usePathname } from "next/navigation";
 import { useTheme } from "next-themes";
 import { RiMoonFill, RiSunLine } from "react-icons/ri";
 import { IoMdMenu, IoMdClose } from "react-icons/io";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
   const [navbarView, setNavbarView] = useState(false);
   const pathname = usePathname();
+
+  const svgVariants = {
+    hidden: { rotate: -180 },
+    visible: {
+      rotate: 0,
+      transition: { duration: 1.5 },
+    },
+  };
+
+  const pathVariants = {
+    hidden: {
+      opacity: 0,
+      pathLength: 0,
+    },
+    visible: {
+      opacity: 1,
+      pathLength: 1,
+      transition: {
+        duration: 3,
+        ease: "easeInOut",
+      },
+    },
+  };
 
   const navItems = [
     {
@@ -30,14 +54,28 @@ const Navbar = () => {
   return (
     <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
       <div className="justify-between md:items-center md:flex">
-        <div>
+        <div className="h-20">
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <Link to="home">
-              <div className="container flex items-center space-x-2">
-                <h2 className="text-2xl font-bold hover:cursor-pointer">
-                  Leon Wellstead
-                </h2>
-              </div>
+            <Link to="home" className="flex align-center">
+              <motion.div className="">
+                <motion.svg
+                  variants={svgVariants}
+                  initial="hidden"
+                  animate="visible"
+                  className="h-20 overflow-visible dark:stroke-white stroke-2 hover:opacity-70 stroke-black"
+                >
+                  <motion.path
+                    fill="none"
+                    variants={pathVariants}
+                    d="M 0 0 M 0 0 L 5 37.5 L 12.5 20 L 20 37.5 L 25 0 M -5 37.5 H -25 V 0"
+                  ></motion.path>
+                  <motion.path
+                    fill="none"
+                    variants={pathVariants}
+                    d="M 0 -10 L -35 -10 V 47.5 H 35 V -10 H 0"
+                  ></motion.path>
+                </motion.svg>
+              </motion.div>
             </Link>
             <div className="md:hidden">
               <button
